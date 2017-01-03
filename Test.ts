9d084cd9132cea8d8c7f1357d5850bb7c2b71fb6
rocampoa <rocampoa@gmail.com>
2017-01-03 13:26:50 -0500

    first commit

---
+++ Test.ts
@@ -0,0 +1,50 @@
+var t : number = 1;
+enum Color {amarillo, azul, rojo};
+var c : Color = Color.azul;
+var list : number[] = [1,2,3];
+var l2 : Array<number> = [3,4,5];
+
+function prueba(b: string = "prueba de valor de parametro") : void {
+  alert("Esto es una prueba" + c + b);
+  if (c == Color.rojo) {
+    alert("Es rojo");
+  }
+  b = 6 + 6;
+}
+
+enum AlertLevel{
+  info,
+  warning,
+  error
+}
+
+function getAlertSubscribers(level : AlertLevel){
+  var emails = new Array<string>();
+  switch(level){
+    case AlertLevel.info:
+       emails.push("cst@domain.com");
+       break;
+    case AlertLevel.warning:
+       emails.push("development@domain.com");
+       emails.push("sysadmin@domain.com");
+       break;
+    case AlertLevel.error:
+      emails.push("development@domain.com");
+      emails.push("sysadmin@domain.com");
+      emails.push("management@domain.com");
+      break;
+    default:
+      throw new Error("Invalid argument!");
+  }
+  return emails;
+}
+
+var greet = (name : string) : string =>  {
+    if(name){
+      return "Hi! " + name;
+    }
+    else
+    {
+      return "Hi! my name is " + this.fullname;
+    }
+};
